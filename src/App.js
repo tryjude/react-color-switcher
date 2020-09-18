7245dff43a00b242f2a3f0e3ba9999389f160917
Jude <tryjude@gmail.com>
2020-09-18 16:16:18 -0400

    Make it smarter, stronger, faster...

--- src/App.js
+++ src/App.js
@@ -6,15 +6,12 @@ import './App.css';
 function App() {
   const [color, setColor] = useState('')
   console.log(setColor)
-  const colors = ['red', 'blue', 'yellow']
+  const colors = ['red', 'blue', 'yellow', 'green']
   return (
     <div className={`react-root ${color}`}>
       <div className="centered">
         <h1>Color Picker</h1>
-        {colors.map(currentColor => <SwitchColorButton color='red' setColor={setColor}/>)}
-        <SwitchColorButton color='red' setColor={setColor} />
-        <SwitchColorButton color='blue' setColor={setColor} />
-        <SwitchColorButton color='yellow' setColor={setColor} />
+        {colors.map(currentColor => <SwitchColorButton color={currentColor} setColor={setColor} key={currentColor} />)}
       </div>
     </div>
   );
